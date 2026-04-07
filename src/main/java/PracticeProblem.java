03d5bd858418571c219edab929117b045a198903
AdhirajSingh-ops <adhiraj.singh3@student.tdsb.on.ca>
2026-04-07 00:14:09 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,19 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int towerOfHanoi(int num) {
+		if (num < 3) {
+			return -1;
+		}
+		return countmoves(num);
+	}
+	public static int countmoves(int n) {
+		if (n == 1) {
+			return 1;
+		}
+		return 2 * countmoves(n - 1) + 1;
+
+	}
 
 }
+
